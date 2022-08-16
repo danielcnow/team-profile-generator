ea95523a20f81a515dd771b56c35c74d1e40bfb1
danielcnow <105885467+danielcnow@users.noreply.github.com>
2022-08-15 21:59:26 -0400

    properties written and jest installed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates a Employee object', () => {
+    const e = new Employee();
+    const e =
+    expect(e.id).toBe(testValue)));
+  });
